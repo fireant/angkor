0ec8a7f53297bbce92e7587af208fc9b4d8359ee
Mosalam <m.ebrahimi@ieee.org>
2011-05-16 21:35:28 +0100

    updated the code to work with the lastest version of libfreenect

--- main.cpp
+++ main.cpp
@@ -52,24 +52,24 @@ int frame = 0;
 using namespace std;
 using namespace osgSim;
 
-void addToLightPointNode ( LightPointNode& lpn, unsigned int noSteps,
-						   unsigned int j, bool bexport , ostringstream& sstream )
+void addToLightPointNode(LightPointNode& lpn, unsigned int noSteps,
+						 unsigned int j, bool bexport, ostringstream& sstream)
 {
-        lpn.getLightPointList().reserve ( noSteps );
+        lpn.getLightPointList().reserve(noSteps);
 
         for ( unsigned int i=0;i<noSteps;++i) {
                 LightPoint lp;
-				lp._color.set ( 1.0f,1.0f,1.0f,1.0f );
+				lp._color.set(1.0f, 1.0f, 1.0f, 1.0f);
                 lp._position.z() = depth_data[j][i] * 14.0;
-                lp._position.y() = (j-120.0) * ( lp._position.z() + -10.0 )
+                lp._position.y() = (j-120.0) * (lp._position.z() + -10.0)
 										* 0.005;
-                lp._position.x() = (i-160.0) * ( lp._position.z() + -10.0 )
+                lp._position.x() = (i-160.0) * (lp._position.z() + -10.0)
 										* 0.005;
 
-                if ( depth_data[j][i] <= ( 100.0/3.33 ) )
+                if (depth_data[j][i] <= ( 100.0/3.33 ))
 						continue;
 				
-				lpn.addLightPoint ( lp );
+				lpn.addLightPoint(lp);
 				
 				if (bexport) {
 						sstream<<(int)lp._position.x()<<" "
@@ -83,8 +83,8 @@ osg::Node* createLightPointsDatabase(ExportState* export_state)
 {
         osg::MatrixTransform* transform = new osg::MatrixTransform;
 
-        transform->setDataVariance ( osg::Object::STATIC );
-        transform->setMatrix ( osg::Matrix::scale ( 0.0002,0.0002,0.0002 ) );
+        transform->setDataVariance(osg::Object::STATIC);
+        transform->setMatrix(osg::Matrix::scale(0.0002, 0.0002, 0.0002));
 
         int noStepsX = 320;
         int noStepsY = 240;
@@ -99,25 +99,24 @@ osg::Node* createLightPointsDatabase(ExportState* export_state)
 				file<<"element vertex ";
 		}
 		
-        pthread_mutex_lock ( &backbuf_mutex );
+        pthread_mutex_lock(&backbuf_mutex);
 
 		ostringstream ss;
 		size_t totall_points = 0;
-        for ( int i=0;i<noStepsY;++i ) {
+        for (int i=0;i<noStepsY;++i) {
 
                 LightPointNode* lpn = new LightPointNode;
 
-                addToLightPointNode ( *lpn, noStepsX, i, export_state->state,
-										ss);
+                addToLightPointNode(*lpn, noStepsX, i, export_state->state, ss);
 				
 				totall_points += lpn->getNumLightPoints();
 
-                transform->addChild ( lpn );
+                transform->addChild(lpn);
         }
-        pthread_mutex_unlock ( &backbuf_mutex );
+        pthread_mutex_unlock(&backbuf_mutex);
 
         osg::Group* group = new osg::Group;
-        group->addChild ( transform );
+        group->addChild(transform);
 		
 		if (export_state->state) {
 				file<<totall_points<<endl;
@@ -134,10 +133,10 @@ osg::Node* createLightPointsDatabase(ExportState* export_state)
         return group;
 }
 
-void depth_cb ( freenect_device *dev, void *v_depth, uint32_t timestamp )
+void depth_cb(freenect_device *dev, void *v_depth, uint32_t timestamp)
 {
-        freenect_depth *depth = ( freenect_depth * ) v_depth;
-        pthread_mutex_lock ( &backbuf_mutex );
+        uint16_t *depth = ( uint16_t * ) v_depth;
+        pthread_mutex_lock(&backbuf_mutex);
         for ( int i=0; i<240; i++ ) {
                 for ( int j=0; j<320; j++ ) {
                         double metric = 100.0 / ( -0.00307 * 
@@ -145,66 +144,77 @@ void depth_cb ( freenect_device *dev, void *v_depth, uint32_t timestamp )
                         depth_data[i][j] = metric;
                 }
         }
-        pthread_mutex_unlock ( &backbuf_mutex );
+        pthread_mutex_unlock(&backbuf_mutex);
         frame++;
 }
 
-void *freenect_threadfunc ( void *arg )
+void *freenect_threadfunc(void *arg)
 {
-        freenect_set_depth_callback ( f_dev, depth_cb );
-        freenect_set_depth_format ( f_dev, FREENECT_FORMAT_11_BIT );
+        freenect_set_depth_callback(f_dev, depth_cb);
+		freenect_set_depth_mode(f_dev, freenect_find_depth_mode(
+			                             FREENECT_RESOLUTION_MEDIUM, 
+										 FREENECT_DEPTH_11BIT));
 
-        freenect_start_depth ( f_dev );
+        int res = freenect_start_depth(f_dev);
+		if (res !=0) {
+			cout<<"freenect_start_depth failed!"<<endl;
+		}
 
-        while ( !die ) {
-                int16_t ax,ay,az;
-                freenect_get_raw_accel ( f_dev, &ax, &ay, &az );
+        while ( !die && freenect_process_events(f_ctx) >= 0 ) {
+                freenect_raw_tilt_state* state;
+				state = freenect_get_tilt_state(f_dev);
                 double dx,dy,dz;
-                freenect_get_mks_accel ( f_dev, &dx, &dy, &dz );
+                freenect_get_mks_accel(state, &dx, &dy, &dz);
                 cout<<"\r frame: "<<frame<<
 						" - mks acc: "<<dx<<" "<<dy<<" "<<dz<<"\r";
         }
 
         cout<<"\nshutting down streams...\n";
 
-        freenect_stop_depth ( f_dev );
+        freenect_stop_depth(f_dev);
 
-        freenect_close_device ( f_dev );
-        freenect_shutdown ( f_ctx );
+        freenect_close_device(f_dev);
+        freenect_shutdown(f_ctx);
 
         cout<<"-- done!\n";
         return NULL;
 }
 
 
-int main ( int argc, char **argv )
+int main(int argc, char **argv)
 {
-        for ( int i=0; i<240; i++ )
-                for ( int j=0; j<320; j++ )
+        for (int i=0; i<240; i++)
+                for (int j=0; j<320; j++)
                         depth_data[i][j] = 0;
 
-        if ( freenect_init ( &f_ctx, NULL ) < 0 ) {
+        if (freenect_init(&f_ctx, NULL) < 0) {
                 cout<<"freenect_init() failed\n";
                 return 1;
         }
 
-        freenect_set_log_level ( f_ctx, FREENECT_LOG_DEBUG );
-        if ( freenect_open_device ( f_ctx, &f_dev, 0 ) < 0 ) {
+        freenect_set_log_level(f_ctx, FREENECT_LOG_DEBUG);
+        if (freenect_open_device(f_ctx, &f_dev, 0) < 0) {
                 cout<<"Could not open device\n";
                 return 1;
         }
         
-        pthread_create ( &freenect_thread, NULL, freenect_threadfunc, NULL );
-
+		int res = pthread_create(&freenect_thread, NULL, freenect_threadfunc,
+								 NULL);
+        if (res) {
+                printf("pthread_create failed\n");
+                return 1;
+        }
+        
         osgViewer::Viewer viewer;
-        viewer.setCameraManipulator ( new osgGA::TrackballManipulator() );
+        viewer.setCameraManipulator (new osgGA::TrackballManipulator());
 		osg::Vec3d eye(0.0,0.0,-1.0);
 		osg::Vec3d center(0.0,0.0,0.0);
 		osg::Vec3d up(0.0,-1.0,0.0);
 		viewer.getCameraManipulator()->setHomePosition(eye, center, up);
-        viewer.setUpViewInWindow ( 640, 0, 640, 480 );
+        viewer.setUpViewInWindow(640, 0, 640, 480);
 		ExportState export_state;
-		TweakBarEventCallback* e_handler = new TweakBarEventCallback(&export_state);
+		TweakBarEventCallback* e_handler;
+		e_handler = new TweakBarEventCallback(&export_state);
 		viewer.addEventHandler(e_handler);
 		osgViewer::ViewerBase::Windows wins;
 		viewer.getWindows(wins);
@@ -218,7 +228,8 @@ int main ( int argc, char **argv )
 		osg::StateSet* ss = geode->getOrCreateStateSet();
 		ss->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
 		
-		osg::Camera* hudCam = cd->createHUD(wins[0]->getTraits()->width, wins[0]->getTraits()->height);
+		osg::Camera* hudCam = cd->createHUD(wins[0]->getTraits()->width,
+											wins[0]->getTraits()->height);
 		hudCam->addChild(geode);
 	
         osg::Group* rootnode = new osg::Group;
@@ -226,18 +237,18 @@ int main ( int argc, char **argv )
 
         osg::Node* lps;
         lps = createLightPointsDatabase(&export_state);
-        rootnode->addChild ( lps );
-        viewer.setSceneData ( rootnode );
+        rootnode->addChild(lps);
+        viewer.setSceneData(rootnode);
 
         while ( !viewer.done() ) {
-                rootnode->removeChild ( lps );
+                rootnode->removeChild(lps);
                 lps = createLightPointsDatabase(&export_state);
-                rootnode->addChild ( lps );
-                viewer.setSceneData ( rootnode );
+                rootnode->addChild(lps);
+                viewer.setSceneData(rootnode);
                 viewer.frame();
         }
         die = 1;
-        sleep ( 1 );
+        sleep(1);
 
         return 0;
 }
